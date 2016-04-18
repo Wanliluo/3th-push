53477985b03cfbdc0d97c052ca6928a444f5df7c
WanliLuo <1367762718@qq.com>
2016-04-18 13:02:56 +0800

    3th commit

---
+++ service/src/main/java/mapper/com/pandawork/mapper/UserMapper.java
@@ -0,0 +1,99 @@
+package com.pandawork.mapper;
+
+import com.pandawork.common.entity.User;
+import com.pandawork.core.common.exception.SSException;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/*
+ **
+ * UserMapper
+ *  Created by chant on 2016/3/23.
+ */
+public interface UserMapper {
+
+    /**
+     *添加用户
+     * @Param user
+     * @return user
+     * @throws Exception
+     **/
+    public void newUser(@Param("user")User user) throws Exception;
+
+    /**
+     * 根据用户名删除用户
+     * @Param userName
+     * @return
+     * @throws Exception
+     *
+     * */
+    public boolean delById(@Param("id")int id)throws Exception;
+
+    /**
+     * 更新用户信息
+     * @param user
+     * @throws Exception
+     */
+    public void update(@Param("user")User user) throws Exception;
+
+    /**
+     * 根据id查User
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    public User queryById(int id) throws Exception;
+
+    /**
+     * 根据userName查找用户
+     * @param userName
+     * @return
+     * @throws Exception
+     */
+    public User queryByUserName(String userName)throws Exception;
+
+    /**
+     * 判断用户是否存在
+     * @param userName
+     * @return
+     * @throws Exception
+     */
+    public Integer  checkUserName(String userName)throws Exception;
+
+    /**
+     * 验证用户名和密码
+     * @param userName
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    public Integer countByNAndPw(@Param("userName") String userName,
+                                 @Param("password") String password) throws Exception ;
+
+    /**
+     * 获取用户列表
+     * @return
+     * @throws Exception
+     */
+    public List<User> listAll() throws Exception;
+
+    /**
+     * 检查登录
+     * @param userName
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    public boolean checkLogin(@Param("userName") String userName,
+                              @Param("password") String password) throws Exception ;
+
+    /**
+     * 动态查询用户
+     * @param userName
+     * @return
+     * @throws Exception
+     */
+    public User selectUserCondition(@Param("userName") String userName) throws Exception;
+
+}
